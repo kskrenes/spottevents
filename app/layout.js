1190fe47defdd38f54f50a104ce84f54744cc041
kskrenes@gmail.com <kskrenes@gmail.com>
2026-02-05 14:48:56 -0700

    install clerk and add to layout

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import Header from "@/components/header";
 import { ConvexClientProvider } from "./ConvexClientProvider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata = {
   title: "Spott",
@@ -18,19 +19,21 @@ export default function RootLayout({ children }) {
           enableSystem
           disableTransitionOnChange
           >
-            <ConvexClientProvider>
-              {/* header */}
-              <Header/>
-              <main className="relative min-h-screen container mx-auto pt-40 md:pt-32">
-                {/* glow */}
-                <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
-                  <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl"/>
-                  <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-orange-600/20 rounded-full blur-3xl"/>
-                </div>
-                <div className="relative z-10">{children}</div>
-                {/* footer */}
-              </main>
-            </ConvexClientProvider>
+            <ClerkProvider>
+              <ConvexClientProvider>
+                {/* header */}
+                <Header/>
+                <main className="relative min-h-screen container mx-auto pt-40 md:pt-32">
+                  {/* glow */}
+                  <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
+                    <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl"/>
+                    <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-orange-600/20 rounded-full blur-3xl"/>
+                  </div>
+                  <div className="relative z-10">{children}</div>
+                  {/* footer */}
+                </main>
+              </ConvexClientProvider>
+            </ClerkProvider>
         </ThemeProvider>
       </body>
     </html>
