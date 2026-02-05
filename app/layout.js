594839f2ffd7d47e8c823feb56466da278d757dc
kskrenes@gmail.com <kskrenes@gmail.com>
2026-02-04 18:33:08 -0700

    implement shadcn dark mode theme

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 
 export const metadata = {
@@ -7,11 +8,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
-        {/* header */}
-        {children}
-        {/* footer */}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          >
+          {/* header */}
+          {children}
+          {/* footer */}
+        </ThemeProvider>
       </body>
     </html>
   );
