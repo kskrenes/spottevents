f5b102026c901f561ddfc875d104333eae9c27f1
kskrenes@gmail.com <kskrenes@gmail.com>
2026-02-06 09:52:05 -0700

    add db schema and ability to store and retrieve users, add loading and authenticated states to header

--- components/header.jsx
+++ components/header.jsx
@@ -7,8 +7,12 @@ import React from 'react'
 import { Button } from './ui/button'
 import { Authenticated, Unauthenticated } from 'convex/react'
 import { BarLoader } from 'react-spinners';
+import { useStoreUser } from '@/hooks/use-store-user';
 
 const Header = () => {
+
+  const { isLoading, isAuthenticated } = useStoreUser();
+
   return (
     <>
       <nav className='fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-xl z-20 border-b'>
@@ -34,9 +38,11 @@ const Header = () => {
         </div>
         {/* Mobile Search and Location - below header */}
         {/* Loading Indicator */}
-        <div className='absolute bottom-0 left-0 w-full'>
-          <BarLoader width={"100%"} color='#A855F7' />
-        </div>
+        {isLoading && (
+          <div className='absolute bottom-0 left-0 w-full'>
+            <BarLoader width={"100%"} color='#A855F7' />
+          </div>
+        )}
       </nav>
       {/* Modals */}
     </>
