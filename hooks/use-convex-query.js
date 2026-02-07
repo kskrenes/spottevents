3c340e2d94eafda93f9fb30b24b09e9354187199
kskrenes@gmail.com <kskrenes@gmail.com>
2026-02-06 20:53:42 -0700

    fix isLoading to initialize to false

--- hooks/use-convex-query.js
+++ hooks/use-convex-query.js
@@ -6,7 +6,7 @@ export const useConvexQuery = (query, ...args) => {
   const result = useQuery(query, ...args);
 
   const [data, setData] = useState(undefined);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
