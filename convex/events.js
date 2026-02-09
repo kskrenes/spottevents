4b13c86e3d21dd76b1fb39d61c73858d43f42e2d
kskrenes@gmail.com <kskrenes@gmail.com>
2026-02-09 10:05:55 -0700

    use relative path for imports in convex

--- convex/events.js
+++ convex/events.js
@@ -1,6 +1,6 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
-import { NUM_FEATURED_EVENTS, NUM_LOCAL_EVENTS, NUM_POPULAR_EVENTS } from "@/lib/layout-utils";
+import { NUM_FEATURED_EVENTS, NUM_LOCAL_EVENTS, NUM_POPULAR_EVENTS } from "../lib/layout-utils";
 
 const createReductionBuckets = () => [[], [], [], []];
 
