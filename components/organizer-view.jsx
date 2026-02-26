0c882ff64d129e47eb641e0499a35f3acc2b4cb9
kskrenes@gmail.com <kskrenes@gmail.com>
2026-02-26 13:33:27 -0700

    use avatar ui component to display organizer avatar

--- components/organizer-view.jsx
+++ components/organizer-view.jsx
@@ -3,8 +3,9 @@ import { useConvexQuery } from '@/hooks/use-convex-query';
 import { Loader2 } from 'lucide-react';
 import Image from 'next/image';
 import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 
-const OrganizerView = ({event}) => {
+const OrganizerView = ({ event }) => {
   
   const { data: organizer, isLoading } = useConvexQuery(
     api.users.getUserById,
@@ -20,10 +21,15 @@ const OrganizerView = ({event}) => {
   }
 
   return (
-    <div className='flex items-center gap-2'>
-      <Image src={organizer.imageUrl} alt={organizer.name} width={50} height={50} />
+    <div className='flex items-center gap-3'>
+      <Avatar className='w-12 h-12'>
+        <AvatarImage src={organizer.imageUrl} />
+        <AvatarFallback>
+          {organizer.name.charAt(0).toUpperCase()}
+        </AvatarFallback>
+      </Avatar>
       <div className='-space-y-1'>
-        <span className='line-clamp-1 text-sm'>{organizer.name}</span>
+        <span className='line-clamp-1 text-sm font-medium'>{organizer.name}</span>
         <span className='text-xs text-muted-foreground'>Event Organizer</span>
       </div>
     </div>
