ab57f0c1a7b83f2e7edf0fcd7e76bb82b241541a
kskrenes@gmail.com <kskrenes@gmail.com>
2026-03-02 15:44:32 -0700

    define helper methods to extract repeated code

---
+++ convex/lib/auth.js
@@ -0,0 +1,24 @@
+export async function getUserByClerkId(ctx, id, requireAuth=true) {
+  let user;
+  if (id) {
+    user = await ctx.db
+      .query("users")
+      .withIndex("by_clerkId", (q) => q.eq("clerkId", id))
+      .unique();
+  }
+  
+  if (requireAuth && !user) {
+    throw new Error("user not found with clerkId ", id);
+  }
+
+  return user;
+}
+
+export async function requireUser(ctx, requireAuth=true) {
+  const identity = await ctx.auth.getUserIdentity();
+  if (requireAuth && !identity) {
+    throw new Error("not authenticated");
+  }
+
+  return await getUserByClerkId(ctx, identity?.subject, requireAuth);
+}
